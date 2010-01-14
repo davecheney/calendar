09403045b4ffa930f29085480e877198b9b92d06
Dave Cheney <dave@cheney.net>
2010-01-14 22:31:56 +1100

    Fixed tests

--- calendar-core/src/test/java/net/cheney/calendar/router/prefix/TestPrefixRouter.java
+++ calendar-core/src/test/java/net/cheney/calendar/router/prefix/TestPrefixRouter.java
@@ -7,8 +7,6 @@ import static junit.framework.Assert.assertFalse;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.Assert;
-
 import net.cheney.motown.uri.Path;
 
 import org.junit.Before;
@@ -18,11 +16,11 @@ import org.junit.Test;
 
 public class TestPrefixRouter {
 	
-	private static PrefixRoutable<Object> PRINCIPALS_USERS = new PrefixRoutable<Object>(Path.fromString("/principals/users"));
-	private static PrefixRoutable<Object> PRINCIPALS_GROUPS = new PrefixRoutable<Object>(Path.fromString("/principals/groups"));
-	private static PrefixRoutable<Object> PRINCIPALS = new PrefixRoutable<Object>(Path.fromString("/principals"));
-	private static PrefixRoutable<Object> CALENDARS = new PrefixRoutable<Object>(Path.fromString("/calendars"));
-	private static PrefixRoutable<Object> GROUPS = new PrefixRoutable<Object>(Path.fromString("/groups"));
+	private static PrefixRoutable<Object> PRINCIPALS_USERS = new PrefixRoutable<Object>(Path.fromString("/principals/users"), "a");
+	private static PrefixRoutable<Object> PRINCIPALS_GROUPS = new PrefixRoutable<Object>(Path.fromString("/principals/groups"), "a");
+	private static PrefixRoutable<Object> PRINCIPALS = new PrefixRoutable<Object>(Path.fromString("/principals"), "a");
+	private static PrefixRoutable<Object> CALENDARS = new PrefixRoutable<Object>(Path.fromString("/calendars"), "a");
+	private static PrefixRoutable<Object> GROUPS = new PrefixRoutable<Object>(Path.fromString("/groups"), "a");
  	
 	private static List<PrefixRoutable<Object>> routables;
 
@@ -43,7 +41,7 @@ public class TestPrefixRouter {
 	
 	@Test 
 	public void testPrincipals() {
-		assertEquals(asList(PRINCIPALS_USERS), router.bestMatchForPrefix(Path.fromString("/principals/users/dave")));
+		assertEquals(PRINCIPALS_USERS, router.bestMatchForPrefix(Path.fromString("/principals/users/dave")));
 		assertFalse(asList(PRINCIPALS_GROUPS).equals(router.bestMatchForPrefix(Path.fromString("/principals/users/dave"))));
 		assertFalse(asList(PRINCIPALS).equals(router.bestMatchForPrefix(Path.fromString("/principals/users/dave"))));
 	}
